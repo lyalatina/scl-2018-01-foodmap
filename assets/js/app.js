f9a1dc0eeee21affdccf5c1e74ad477f91e98d65
Natalia <nataliamp87@gmail.com>
2018-07-26 12:43:14 -0400

    viendo si funciona geolocalizacion

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,12 @@
 //Splash
-setTimeout(function hide() { $('#pag1').hide('fast');
-document.getElementById('pag2').style.display = 'block'; }, 3000);
+setTimeout(function hide() {
+  $('#pag1').hide('fast');
+  document.getElementById('pag2').style.display = 'block';}, 3000);
 
-
+  var map, infoWindow;
 function initAutocomplete() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -33.48004501644191, lng: -70.6811756526364},
+    center: { lat: -33.48004501644191, lng: -70.6811756526364 },
     zoom: 13,
     mapTypeId: 'roadmap',
     types: ['restaurant']
@@ -17,14 +18,14 @@ function initAutocomplete() {
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
   // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
+  map.addListener('bounds_changed', function () {
     searchBox.setBounds(map.getBounds());
   });
 
   var markers = [];
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
-  searchBox.addListener('places_changed', function() {
+  searchBox.addListener('places_changed', function () {
     var places = searchBox.getPlaces();
 
     if (places.length == 0) {
@@ -32,14 +33,14 @@ function initAutocomplete() {
     }
 
     // Clear out the old markers.
-    markers.forEach(function(marker) {
+    markers.forEach(function (marker) {
       marker.setMap(null);
     });
     markers = [];
 
     // For each place, get the icon, name and location.
     var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
+    places.forEach(function (place) {
       if (!place.geometry) {
         console.log("Returned place contains no geometry");
         return;
@@ -71,60 +72,75 @@ function initAutocomplete() {
   });
 }
 
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else { 
-        x.innerHTML = "Geolocation is not supported by this browser.";
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    var pos = {
+      lat: -33.48004501644191,
+      lng: -70.6811756526364
+    };
+
+    infoWindow.setPosition(pos);
+    infoWindow.setContent('Location found.');
+    infoWindow.open(map);
+    map.setCenter(pos);
+  }, function () {
+    handleLocationError(true, infoWindow, map.getCenter());
+  });
+} else {
+  // Browser doesn't support Geolocation
+  handleLocationError(false, infoWindow, map.getCenter());
+}
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
+/*
+function initMap() {
+// Creamos un mapa con las coordenadas actuales
+navigator.geolocation.getCurrentPosition(function(pos) {
+lat = -33.48004501644191;
+lon = -70.6811756526364;
+
+let myLatlng = new google.maps.LatLng(lat, lon);
+
+let mapOptions = {
+  center: myLatlng,
+  zoom: 14,
+  mapTypeId: google.maps.MapTypeId.SATELLITE,
+};
+
+map = new google.maps.Map(document.getElementById('mapa'), mapOptions);
+
+// Creamos el infowindow
+infowindow = new google.maps.InfoWindow();
+
+// Especificamos la localización, el radio y el tipo de lugares que queremos obtener
+let request = {
+  location: myLatlng,
+  radius: 5000,
+  types: ['restaurant'],
+};
+
+// Creamos el servicio PlaceService y enviamos la petición.
+let service = new google.maps.places.PlacesService(map);
+
+service.nearbySearch(request, function(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (let i = 0; i < results.length; i++) {
+      crearMarcador(results[i]);
     }
-}
-
-function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude;
-}
-   /*
- function initMap() {
- // Creamos un mapa con las coordenadas actuales
-   navigator.geolocation.getCurrentPosition(function(pos) {
-   lat = -33.48004501644191;
-   lon = -70.6811756526364;
-
-   let myLatlng = new google.maps.LatLng(lat, lon);
-
-   let mapOptions = {
-     center: myLatlng,
-     zoom: 14,
-     mapTypeId: google.maps.MapTypeId.SATELLITE,
-   };
-
-   map = new google.maps.Map(document.getElementById('mapa'), mapOptions);
-
-   // Creamos el infowindow
-   infowindow = new google.maps.InfoWindow();
-
-   // Especificamos la localización, el radio y el tipo de lugares que queremos obtener
-   let request = {
-     location: myLatlng,
-     radius: 5000,
-     types: ['restaurant'],
-   };
-
-   // Creamos el servicio PlaceService y enviamos la petición.
-   let service = new google.maps.places.PlacesService(map);
-
-   service.nearbySearch(request, function(results, status) {
-     if (status === google.maps.places.PlacesServiceStatus.OK) {
-       for (let i = 0; i < results.length; i++) {
-         crearMarcador(results[i]);
-       }
-     }
-   });
- });
+  }
+});
+});
 }
 */
 
- 
+
 /*
 
    var zomato = require('zomato');
@@ -163,10 +179,10 @@ rapid.call('Zomato', 'getAllCategories', {
 
 
 fetch(`https://developers.zomato.com/c9358b7a8ecbb24bb098d048eda9cb7b/v2.1/geocode?lat=-33.4600915&lon=-70.6550365`) // t=${movie}(titulo)
-    .then(response => response.json())  //si la peticion es correcta va a dar la informacion como una promesa
-    .then(data => {
-      console.log(data);
-      renderInfo(data);
-    })
-  
+  .then(response => response.json())  //si la peticion es correcta va a dar la informacion como una promesa
+  .then(data => {
+    console.log(data);
+    renderInfo(data);
+  })
+
 
